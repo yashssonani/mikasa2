79ba801c3aeb8451922b5b53c9cadbedab7b8d99
yashssonani <49947849+yashssonani@users.noreply.github.com>
2020-04-14 19:43:09 +0530

    Add files via upload

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -25,7 +25,7 @@ from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, aria
 from tobrot.helper_funcs.download_from_link import request_download
 from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
-
+from tobrot.helper_funcs.pluralsight import plu_dl
 
 async def incoming_message_f(client, message):
     """/leech command"""
@@ -42,35 +42,54 @@ async def incoming_message_f(client, message):
     LOGGER.info(dl_url)
     LOGGER.info(cf_name)
     if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
-        # start the aria2c daemon
-        aria_i_p = await aria_start()
-        LOGGER.info(aria_i_p)
-        current_user_id = message.from_user.id
-        # create an unique directory
-        new_download_location = os.path.join(
-            DOWNLOAD_LOCATION,
-            str(current_user_id),
-            str(time.time())
-        )
-        # create download directory, if not exist
-        if not os.path.isdir(new_download_location):
-            os.makedirs(new_download_location)
-        await i_m_sefg.edit_text("trying to download")
-        # try to download the "link"
-        sagtus, err_message = await call_apropriate_function(
-            aria_i_p,
-            dl_url,
-            new_download_location,
-            i_m_sefg,
-            is_zip,
-            is_unzip
-        )
-        if not sagtus:
-            # if FAILED, display the error message
-            await i_m_sefg.edit_text(err_message)
-    else:
-        await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
+        if "pluralsight.com" in dl_url:
+            await i_m_sefg.edit_text("extracting links")
+            aria_i_p = await aria_start()
+            LOGGER.info(aria_i_p)
+            current_user_id = message.from_user.id
+            new_download_location = os.path.join(
+                DOWNLOAD_LOCATION,
+                str(current_user_id),
+                str(time.time())
+            )
+            # create download directory, if not exist
+            if not os.path.isdir(new_download_location):
+                os.makedirs(new_download_location)
+            await i_m_sefg.edit_text("trying to download")
+
+            plu_dl(dl_url,
+            i_m_sefg
+            )
+        else:
+            await i_m_sefg.edit_text("extracting links")
+            # start the aria2c daemon
+            aria_i_p = await aria_start()
+            LOGGER.info(aria_i_p)
+            current_user_id = message.from_user.id
+            # create an unique directory
+            new_download_location = os.path.join(
+                DOWNLOAD_LOCATION,
+                str(current_user_id),
+                str(time.time())
+            )
+            # create download directory, if not exist
+            if not os.path.isdir(new_download_location):
+                os.makedirs(new_download_location)
+            await i_m_sefg.edit_text("trying to download")
+            # try to download the "link"
+            sagtus, err_message = await call_apropriate_function(
+                aria_i_p,
+                dl_url,
+                new_download_location,
+                i_m_sefg,
+                is_zip,
+                is_unzip
+            )
+            if not sagtus:
+                # if FAILED, display the error message
+                await i_m_sefg.edit_text(err_message)
+        else:
+            await i_m_sefg.edit_text("**FCUK**! wat have you entered. Please read /help")
 
 
 async def incoming_youtube_dl_f(client, message):
